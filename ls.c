e7a564a1f83eb493ff62bdc69d97442cb7f34674
Lazare Rossillon <Lazare@Lazares-MacBook-Pro.local>
2017-06-20 17:12:06 +0200

    coucou

--- ls.c
+++ ls.c
@@ -9,41 +9,35 @@
 /*   Updated: 2017/06/19 18:39:23 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "ls.h"
 #include "libft.h"
 
-int	ft_ls(char *foldername)
+int	ft_store(char *foldername, t_ls	**stock)
 {
 	DIR *dir;
 	struct dirent *dent;
 	dir = opendir(foldername);
-	//struct stat buf;
-	static t_ls *stock = NULL;
 	t_ls *tmp;
 
 	if(dir != NULL)
 	{
-		while ((dent = readdir(dir)) != NULL)
-		{
 			while((dent = readdir(dir)) != NULL)
 			{
 				if (!(stock))
 				{
-					stock = malloc(sizeof(t_ls));
-					stock->next = NULL;
+					*stock = malloc(sizeof(t_ls));
+					(*stock)->next = NULL;
 				}
 				else 
 				{
 					tmp = malloc(sizeof(t_ls));
-					tmp->next = stock;
-					stock = tmp;
+					tmp->next = *stock;
+					*stock = tmp;
 				}
-				stock->name = ft_strnew(ft_strlen(dent->d_name));
-				stock->name = ft_strdup(dent->d_name);
-				stat(stock->name, &stock->stat);
-				printf("%s", stock->name);
-				printf("%llu\n", stock->stat.st_size);
-			}
+				(*stock)->name = ft_strnew(ft_strlen(dent->d_name));
+				(*stock)->name = ft_strdup(dent->d_name);
+				stat((*stock)->name, &((*stock)->stat));
 		}
 		if (closedir(dir))
 			return (-1);
@@ -51,7 +45,21 @@ int	ft_ls(char *foldername)
 	return (0);
 }
 
+void ft_print_normal(t_ls *stock)
+{
+	while (stock && stock->next)
+	{
+		printf("%s\n", (stock)->name);
+		stock = (stock)->next;
+	}
+	if (stock)
+		printf("%s\n", (stock)->name);
+}
+
 int main()
 {
-	ft_ls(".");
+	t_ls *stock = NULL;
+	ft_store(".", &stock);
+	ft_print_normal(stock);
+	return (0);
 }
