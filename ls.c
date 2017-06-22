ff34926caf36c4519005132ec664e7e9c5709081
Lazare ROSSILLON <lazrossi@e1r7p3.42.fr>
2017-06-22 17:20:22 +0200

    coucou

--- ls.c
+++ ls.c
@@ -6,16 +6,16 @@
 /*   By: lazrossi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/18 19:08:21 by lazrossi          #+#    #+#             */
-/*   Updated: 2017/06/21 17:20:21 by lazrossi         ###   ########.fr       */
+/*   Updated: 2017/06/22 17:15:05 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ls.h"
 #include "libft.h"
 
-int ft_print_normal(t_ls **stock);
+int ft_print_normal(t_ls *stock);
 
-int	ft_store(char *foldername, t_ls	*(*stock))
+t_ls	*ft_store(char *foldername, t_ls	*stock)
 {
 	DIR *dir;
 	struct dirent *dent;
@@ -26,58 +26,55 @@ int	ft_store(char *foldername, t_ls	*(*stock))
 	{
 		while((dent = readdir(dir)) != NULL)
 		{
-			if (!(stock))
+			if (!stock)
 			{
-				(*stock) = malloc(sizeof(t_ls));
-				((*stock))->next = NULL;
+				stock = malloc(sizeof(t_ls));
+				(stock)->next = NULL;
 			}
 			else 
 			{
 				tmp = malloc(sizeof(t_ls));
-				tmp->next = (*stock);
-				(*stock) = tmp;
+				tmp->next = stock;
+				stock = tmp;
 			}
-			((*stock))->name = ft_strnew(ft_strlen(dent->d_name));
-			((*stock))->name = ft_strdup(dent->d_name);
-			stat(((*stock))->name, &(((*stock))->stat));
+			(stock)->name = ft_strnew(ft_strlen(dent->d_name));
+			(stock)->name = ft_strdup(dent->d_name);
+			stat(stock->name, &(stock->stat));
 		}
 		if (closedir(dir))
-			return (-1);
+			return (NULL);
 	}
-	while (!(ft_print_normal(stock)))
-		ft_print_normal(stock);
-	return (0);
+	 ft_print_normal(stock);
+	return (stock);
 }
 
-int ft_print_normal(t_ls **stock)
+int ft_print_normal(t_ls *stock)
 {
-	if((strcmp((*stock)->name,".")==0 || strcmp((*stock)->name,"..")==0 || (*(*stock)->name) == '.' ))
+	while (stock && stock->next)
 	{
+		if((strcmp(stock->name,".")==0 || strcmp(stock->name,"..")==0 || (*stock->name) == '.' ))
+		{
+		}
+		else 
+			printf("%s\n", stock->name);
+		stock = stock->next;
 	}
-	else 
-		printf("%s\n", ((*stock))->name);
-	(*stock) = ((*stock))->next;
-	if (*stock && (*stock)->next)
-		return (0);
-	if ((*stock) && (!(strcmp((*stock)->name,".")==0 || strcmp((*stock)->name,"..")==0 || (*(*stock)->name) == '.' )))
-		printf("%s\n", ((*stock))->name);
 	return (1);
 }
 
 int main()
 {
-	t_ls (**stock) = NULL;
-	ft_store(".", stock);
-	if (!(stock))
-		printf("%s\n", "coucou");
-	while (*stock && (*stock)->next)
+	t_ls *stock = NULL;
+	t_ls *new = NULL;
+	stock = ft_store(".", stock);
+	while (stock)
 	{
-		printf("%s\n", "coucou");
-
-		if (S_ISDIR((*stock)->stat.st_mode) && (!(strcmp((*stock)->name,".")==0 || strcmp((*stock)->name,"..")==0 || (*(*stock)->name) == '.' )))
-			ft_store((*stock)->name, stock);
-		*stock = (*stock)->next;
-
+	  if ( S_ISDIR(stock->stat.st_mode) && (!(strcmp(stock->name,".")==0 || strcmp(stock->name,"..")==0 || (*stock->name) == '.' )))
+		return (ft_store(stock->name, new));
+	  if (stock->next)
+		  stock = stock->next;
+	  else
+		  break;
 	}
 	return (0);
 }
