efda37b788e21868424bdce9d427cc0f63b929e3
lazrossi <lazrossi@student.42.fr>
2017-09-01 10:29:23 +0200

    sort quasiment fonctionnel des options semblent ne pas marcher mais ca ne segfault pas

--- srcs/ft_place_element.c
+++ srcs/ft_place_element.c
@@ -14,19 +14,34 @@
 
 int		ft_sort_by_options(t_ls *stock, t_ls *next_stock, int sort_options)
 {
-	if (sort_options & CMD_T)
-		return (stock->stat.st_mtimespec.tv_sec - next_stock->stat.st_mtimespec.tv_sec);
-	else if (sort_options & CMD_LAST_ACCESS)
-		return (stock->stat.st_atimespec.tv_sec - next_stock->stat.st_atimespec.tv_sec);
-	else if (sort_options & CMD_S)
-		return (stock->stat.st_size - next_stock->stat.st_size);
-	else if (sort_options & CMD_U)
-		return (stock->stat.st_birthtimespec.tv_sec - next_stock->stat.st_birthtimespec.tv_sec);
+	if (!(sort_options & CMD_REVERSE))
+	{
+		if (sort_options & CMD_T)
+			return (stock->stat.st_mtimespec.tv_sec - next_stock->stat.st_mtimespec.tv_sec);
+		else if (sort_options & CMD_LAST_ACCESS)
+			return (stock->stat.st_atimespec.tv_sec - next_stock->stat.st_atimespec.tv_sec);
+		else if (sort_options & CMD_S)
+			return (stock->stat.st_size - next_stock->stat.st_size);
+		else if (sort_options & CMD_U)
+			return (stock->stat.st_birthtimespec.tv_sec - next_stock->stat.st_birthtimespec.tv_sec);
+		else
+			return (ft_strcmp(stock->name, next_stock->name));
+	}
 	else
-		return (ft_strcmp(stock->name, next_stock->name));
+		if (sort_options & CMD_T)
+			return (next_stock->stat.st_mtimespec.tv_sec - stock->stat.st_mtimespec.tv_sec);
+		else if (sort_options & CMD_LAST_ACCESS)
+			return (next_stock->stat.st_atimespec.tv_sec - stock->stat.st_atimespec.tv_sec);
+		else if (sort_options & CMD_S)
+			return (next_stock->stat.st_size - stock->stat.st_size);
+		else if (sort_options & CMD_U)
+			return (next_stock->stat.st_birthtimespec.tv_sec - stock->stat.st_birthtimespec.tv_sec);
+		else
+			return (ft_strcmp(next_stock->name, stock->name));
 }
 
-int		ft_place_rev_elem(t_ls *stock, t_ls *next_stock, const char *foldername, int **max, int sort_options)
+int		ft_place_elem(t_ls *stock, t_ls *next_stock,
+		const char *foldername, int **max, int sort_options)
 {
 	if (ft_sort_by_options(stock, next_stock, sort_options) <= 0)
 	{
@@ -38,7 +53,7 @@ int		ft_place_rev_elem(t_ls *stock, t_ls *next_stock, const char *foldername, in
 			return (1);
 		}
 		else
-			return (ft_place_rev_elem(stock->left, next_stock, foldername, max, sort_options));
+			return (ft_place_elem(stock->left, next_stock, foldername, max, sort_options));
 	}
 	if (!stock->right)
 	{
@@ -48,37 +63,5 @@ int		ft_place_rev_elem(t_ls *stock, t_ls *next_stock, const char *foldername, in
 		return (1);
 	}
 	else
-		return (ft_place_rev_elem(stock->right, next_stock, foldername, max, sort_options));
-}
-
-int		ft_place_elem(t_ls *stock, t_ls *next_stock,
-		const char *foldername, int **max, int sort_options)
-{
-	if (!(sort_options & CMD_REVERSE))
-	{
-		if (ft_sort_by_options(stock, next_stock, sort_options) <= 0)
-		{
-			if (!stock->left)
-			{
-				stock->left = next_stock; 
-				if (stock->color == 0)
-					return (0);
-				return (1);
-			}
-			else
-				return (ft_place_elem(stock->left, next_stock, foldername, max, sort_options));
-		}
-		if (!stock->right)
-		{
-			stock->right = next_stock; 
-			if (stock->color == 0)
-				return (0);
-			return (1);
-		}
-		else
-			return (ft_place_elem(stock->right, next_stock, foldername, max, sort_options));
-	}
-	else
-		return (ft_place_rev_elem(stock, next_stock, foldername, max, sort_options));
-	return (1);
+		return (ft_place_elem(stock->right, next_stock, foldername, max, sort_options));
 }
