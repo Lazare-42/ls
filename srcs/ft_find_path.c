e78d54ed6836165364e3d816db9b49a4f2a585a6
Lazare ROSSILLON <lazrossi@e3r2p5.42.fr>
2017-09-02 17:23:09 +0200

    liens symboliques fonctionnels

--- srcs/ft_find_path.c
+++ srcs/ft_find_path.c
@@ -6,7 +6,7 @@
 /*   By: lazrossi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 13:43:36 by lazrossi          #+#    #+#             */
-/*   Updated: 2017/09/02 17:10:14 by lazrossi         ###   ########.fr       */
+/*   Updated: 2017/09/02 17:22:48 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*find_path(char *name, char *foldername)
 {
 	char *path;
 
-	path = ft_strjoin(foldername, name);
+	path = ft_strjoin(foldername, "/");
+	path = ft_strjoinfree(&path, &name, 'L');
 	return (path);
 }
